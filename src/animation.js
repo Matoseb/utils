ba41a758c0be8998c8591ee3d008e1c73349921f
Sébastien Matos <matos.sebastien@gmail.com>
2022-03-16 19:35:20 +0100

    Add smoothen path + damper

--- src/animation.js
+++ src/animation.js
@@ -1,3 +1,5 @@
+import { clamp } from './math'
+
 export class Spring {
   constructor(options = {}) {
     this.settings = {
@@ -11,7 +13,7 @@ export class Spring {
     this.position = this.target = this.settings.position
   }
 
-  target(target) {
+  follow(target) {
     this.target = target
   }
 
@@ -34,40 +36,53 @@ export class Spring {
   }
 }
 
-export default class Smoother {
-  constructor() {}
-  //   constructor(options = {}) {
-  //     this.settings = {
-  //       position: 0,
-  //       drag: 0.75,
-  //       strength: 0.1,
-  //       ...options
-  //     }
+export class Smoothdamper {
+  constructor(options = {}) {
+    this.settings = {
+      value: 0,
+      smoothness: 1,
+      maxSpeed: Infinity,
+      ...options,
+    }
 
-  //     this.velocity = 0
-  //     this.position = this.target = this.settings.position
-  //   }
+    this.velocity = 0
+    this.value = this.settings.value
+  }
 
-  //   target(target) {
-  //     this.target = target
-  //   }
+  setTarget(target) {
+    this.target = target
+  }
 
-  //   update(target = this.target) {
-  //     const { strength, drag } = this.settings
+  update(deltaTime) {
+    this.value = this.smoothDamp(this.value, this.target, deltaTime)
+    return this.value
+  }
 
-  //     let force = target - this.position
-  //     force *= strength
+  valueOf() {
+    return this.value
+  }
 
-  //     this.velocity *= drag
-  //     this.velocity += force
-
-  //     this.position += this.velocity
-
-  //     return this.position
-  //   }
-
-  //   valueOf() {
-  //     return this.position
-  //   }
-  //
+  smoothDamp(
+    current,
+    target,
+    deltaTime
+  ) {
+    const { smoothness, maxSpeed } = this.settings
+    let num = 2 / (smoothness || 0.00001);
+    let num2 = num * deltaTime;
+    let num3 = 1 / (1 + num2 + 0.48 * num2 * num2 + 0.235 * num2 * num2 * num2);
+    let num4 = current - target;
+    let num5 = target;
+    let num6 = maxSpeed * smoothness;
+    num4 = clamp(num4, -num6, num6);
+    target = current - num4;
+    let num7 = (this.velocity + num * num4) * deltaTime;
+    this.velocity = (this.velocity - num * num7) * num3;
+    let num8 = target + (num4 + num7) * num3;
+    if (num5 - current > 0 === num8 > num5) {
+      num8 = num5;
+      this.velocity = (num8 - num5) / deltaTime;
+    }
+    return num8;
+  }
 }
