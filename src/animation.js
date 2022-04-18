3c2f2dfe9764547f45467bb3e49e7f0c5f2086d5
Sébastien Matos <matos.sebastien@gmail.com>
2022-04-19 00:50:32 +0200

    Fix clock depedency

--- src/animation.js
+++ src/animation.js
@@ -73,7 +73,7 @@ export class SmoothDamper extends Smoother {
     this.clock = new Clock()
   }
 
-  update(deltaTime = this.clock.tick(), target) {
+  update(deltaTime = this.clock.delta(), target) {
     super.setTarget(target)
     const { smoothness, maxSpeed } = this.settings
     const results = smoothDamp(
