475122da064a0f6acd3fd24a7d9d501a173e8aa1
Sébastien Matos <matos.sebastien@gmail.com>
2022-05-08 14:24:32 +0200

    Add symbol

--- website/App.vue
+++ website/App.vue
@@ -99,8 +99,8 @@ export default {
     searchPlaceholder() {
       const shortcut = platform.mouse
         ? platform.macos
-          ? 'cmd+f'
-          : 'ctrl+f'
+          ? '⌘F'
+          : 'Ctrl F'
         : ''
 
       return `Search ${shortcut}`
