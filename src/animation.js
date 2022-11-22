250d2ff643ad31c3095d975e348792cae52f0cf9
Sébastien Matos <matos.sebastien@gmail.com>
2022-11-22 20:19:57 +0100

    Add step function

--- src/animation.js
+++ src/animation.js
@@ -1,4 +1,4 @@
-import { lerp, smoothDamp } from './math'
+import { lerp, smoothDamp, mapClamped, modulo } from './math'
 import { Clock } from './time'
 
 export class Smoother {
@@ -88,4 +88,30 @@ export class SmoothDamper extends Smoother {
     this.velocity = results.velocity
     return this.value
   }
+}
+
+// https://morethandev.hashnode.dev/demystifying-the-cubic-bezier-function-ft-javascript
+export function cubicBezier(t, initial, p1, p2, final) {
+  return (
+    (1 - t) * (1 - t) * (1 - t) * initial +
+    3 * (1 - t) * (1 - t) * t * p1 +
+    3 * (1 - t) * t * t * p2 +
+    t * t * t * final
+  )
+}
+
+function ease(t, easing, tolerance) {
+  tolerance *= 0.5
+  t = mapClamped(t, tolerance, 1 - tolerance, 0, 1)
+  // return (1 - (Math.cos(t * PI) * 0.5 + 0.5))
+  return cubicBezier(t, easing, 0, 1 - easing, 1)
+}
+
+export function snap(
+  value,
+  dist,
+  { start = 0, tolerance = 0, easing = 0.5 } = {}
+) {
+  let t = (value + start) / dist
+  return (Math.floor(t) + ease(modulo(t, 1), easing, tolerance)) * dist - start
 }
